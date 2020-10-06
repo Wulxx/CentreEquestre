a1c76dd6d3c53db5958c71a33085726bd5110c4e
Alexendre-lx <alexendre.obli@gmail.com>
2020-10-07 00:00:57 +0200

    update back

--- back/index.js
+++ back/index.js
@@ -1,1 +1,11 @@
-let express = require('express') 
+const express = require('express')
+const app = express()
+const port = 4042
+
+app.get('/', (req, res) => {
+  res.send('Centre equestre ')
+})
+
+app.listen(port, () => {
+  console.log(`Nous écoutons sur le port http://localhost:${port}`)
+})
