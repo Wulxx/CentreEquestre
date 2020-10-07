42e58d5e99d4434cfe0f3e563c52bd15f09e4054
Alexendre-lx <alexendre.obli@gmail.com>
2020-10-07 17:20:10 +0200

    Initial commit

---
+++ back/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express()
+const port = 4042;
+
+
+app.use(bodyParser.json());
+
+app.get('/', (req,res) => {
+  res.send('Test')
+
+})
+
+app.listen(port, () => {
+  console.log(`Nous écoutons sur le port http://localhost:${port}`)
+}) 
