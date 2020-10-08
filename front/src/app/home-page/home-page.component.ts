43b6020d500878476dd4eea3cb397183c983e1db
Wulxx <yoan.dlvx@hotmail.fr>
2020-10-08 15:28:55 +0200

    Actualization

--- front/src/app/home-page/home-page.component.ts
+++ front/src/app/home-page/home-page.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { first } from 'rxjs/operators';
 
-@Component({
-  selector: 'app-home-page',
-  templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
-})
+import { User } from '../user'; /* A modifier plus tard avec le back*/
+import { UserService } from '../user.service'; /* A modifier plus tard avec le back*/
+@Component({ templateUrl: 'home-page.component.html' })
 
-export class HomePageComponent implements OnInit {
+export class HomePageComponent {
+    loading = false;
+    users: User[];
 
-  constructor() { }
+    constructor(private userService: UserService) { }
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit() {
+        this.loading = true;
+        this.userService.getAll().pipe(first()).subscribe(users => {
+            this.loading = false;
+            this.users = users;
+        });
+    }
 }
