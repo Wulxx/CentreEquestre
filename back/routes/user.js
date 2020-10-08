a983f9b0c7e610bf0a8d608a50f0e9fdfb1084e0
Alexendre-lx <alexendre.obli@gmail.com>
2020-10-08 13:13:36 +0200

    update Front

--- back/routes/user.js
+++ back/routes/user.js
@@ -1,7 +1,21 @@
 import express from 'express';
 
+
+import { createUser, getUser, updateUser, deleteUser } from '../controllers/users.js'
+
 const router = express.Router();
 
-router.get('/users', (req,res) => {
-    res.send('comeBack')
+
+router.get('/', (req,res) => {
+    res.send(users)
 })
+
+router.post('/', createUser)
+
+router.get('/:id',getUser )
+
+router.delete('/:id',updateUser)
+
+router.patch('/:id', deleteUser)
+
+export default router;
